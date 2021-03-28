3da8a5c7a5c9745e426d43fb7b6a46a704f379e3
alexeylozenko <alexeylozenko@gmail.com>
2021-03-28 18:40:06 +0300

    refactored function in brain-gcd

--- src/brain-gcd/__tests__/brain-gcd.test.js
+++ src/brain-gcd/__tests__/brain-gcd.test.js
@@ -1,18 +1,18 @@
-import { greatestCommonDivisor } from '../brain-gcd.js';
+import { biggestDivider } from '../brain-gcd.js';
 
 describe('brain-gcd', () => {
   it('greatestCommonDivisor', () => {
     let firstNumber = 17;
-    expect(greatestCommonDivisor(firstNumber)).toBe(17);
+    expect(biggestDivider(firstNumber)).toBe(17);
     firstNumber = 15;
-    expect(greatestCommonDivisor(firstNumber)).toBe(5);
+    expect(biggestDivider(firstNumber)).toBe(5);
     firstNumber = 20;
-    expect(greatestCommonDivisor(firstNumber)).toBe(10);
+    expect(biggestDivider(firstNumber)).toBe(10);
     firstNumber = 11;
-    expect(greatestCommonDivisor(firstNumber)).toBe(11);
+    expect(biggestDivider(firstNumber)).toBe(11);
     firstNumber = 7;
-    expect(greatestCommonDivisor(firstNumber)).toBe(7);
+    expect(biggestDivider(firstNumber)).toBe(7);
     firstNumber = 8;
-    expect(greatestCommonDivisor(firstNumber)).toBe(4);
+    expect(biggestDivider(firstNumber)).toBe(4);
   });
 });
