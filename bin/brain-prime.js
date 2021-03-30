21a6479b098177523695908e976b4b57abf0cc5a
alexeylozenko <alexeylozenko@gmail.com>
2021-03-30 17:39:46 +0300

    change implementation in bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-import askUser from '../src/cli/cli.js';
+import { greetingsGame } from '../src/cli/cli.js';
 import brainPrime from '../src/brain-prime/brain-prime.js';
 
-console.log('Welcome to the Brain Games!');
-const name = askUser('May I have your name? ');
-console.log(`Hello, ${name}!`);
-
-brainPrime(name);
+brainPrime(greetingsGame());
