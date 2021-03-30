18aab7745f7d20d87383739d6dca07f4d5a4c204
alexeylozenko <alexeylozenko@gmail.com>
2021-03-30 17:42:26 +0300

    change implementation game in bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-import askUser from '../src/cli/cli.js';
+import { greetingsGame } from '../src/cli/cli.js';
 import brainProgression from '../src/brain-progression/brain-progression.js';
 
-console.log('Welcome to the Brain Games!');
-const name = askUser('May I have your name? ');
-console.log(`Hello, ${name}`);
-
-brainProgression(name);
+brainProgression(greetingsGame());
