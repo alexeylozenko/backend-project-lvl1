b6f4f34258fae37783298cb60e2117db39747b07
alexeylozenko <alexeylozenko@gmail.com>
2021-03-30 17:35:13 +0300

    change game bin/brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-import askUser from '../src/cli/cli.js';
+import { greetingsGame } from '../src/cli/cli.js';
 import brainCalc from '../src/brain-calc/brain-calc.js';
 
-console.log('Welcome to the Brain Games!');
-const name = askUser('May I have your name? ');
-console.log(`Hello, ${name}`);
-
-brainCalc(name);
+brainCalc(greetingsGame());
