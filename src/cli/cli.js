d493d1766f00b177cc96aa34e93223282e2b8c22
alexeylozenko <alexeylozenko@gmail.com>
2021-03-30 17:28:27 +0300

    change of implementation in src/cli/cli.js

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 /**
  *
  * @param questionText
@@ -6,3 +7,14 @@ import readlineSync from 'readline-sync';
  */
 const askUser = (questionText) => readlineSync.question(questionText);
 export default askUser;
+
+/**
+ * @param {string} textGreetings
+ * @returns {string|boolean}
+ */
+export const greetingsGame = (textGreetings = 'Welcome to the Brain Games!') => {
+  console.log(textGreetings);
+  const name = askUser('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  return name;
+};
