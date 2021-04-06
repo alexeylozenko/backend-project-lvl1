09381fe4dc13f5d96698f7768934d862b13c5745
alexeylozenko <alexeylozenko@gmail.com>
2021-04-06 12:29:56 +0300

    fixed greetings of user in bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,4 @@ import askUser from '../src/cli/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = askUser('May I have your name? ');
-console.log(`Hello, ${name}`);
+console.log(`Hello, ${name}!`);
