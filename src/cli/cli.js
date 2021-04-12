98a95dc58a9c43af15dca61d7bdf7b5e8fb7c04d
alexeylozenko <alexeylozenko@gmail.com>
2021-04-12 03:12:55 +0300

    full changed module cli

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,20 +1,49 @@
 import readlineSync from 'readline-sync';
 
 /**
- *
- * @param questionText
+ * @param {string} questionText
  * @return {string | boolean}
  */
-const askUser = (questionText) => readlineSync.question(questionText);
-export default askUser;
+export const askUser = (questionText) => readlineSync.question(questionText);
 
 /**
- * @param {string} textGreetings
- * @returns {string|boolean}
+ *
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
  */
-export const greetingsGame = (textGreetings = 'Welcome to the Brain Games!') => {
-  console.log(textGreetings);
-  const name = askUser('May I have your name? ');
+export const getRandomNumber = (min = 1, max = 21) => {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+};
+
+/**
+ * @param {object} gameModule
+ * @param {number} quantityQuestions
+ */
+export const runCLI = (gameModule, quantityQuestions = 3) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
+  const descriptionGame = gameModule.getDescriptionGame();
+  console.log(descriptionGame);
+
+  for (let i = 0; i < quantityQuestions; i += 1) {
+    const expression = gameModule.generateTask();
+    console.log(`Question: ${expression}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const normalizedAnswer = userAnswer.toLowerCase();
+    const totalResultOfGame = gameModule.playGame(expression, normalizedAnswer);
+    if (totalResultOfGame !== 'Correct!') {
+      console.log(totalResultOfGame);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+    if (i === quantityQuestions - 1) {
+      console.log(`Congratulations, ${name}!`);
+      break;
+    }
+    console.log(totalResultOfGame);
+  }
 };
