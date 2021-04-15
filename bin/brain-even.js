ae8434c3bcb8c4600e4587bdf71c7a76a164afa2
alexeylozenko <alexeylozenko@gmail.com>
2021-04-15 17:05:41 +0300

    fixed bin games

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as gameModule  from '../src/games/brain-even.js';
+import * as gameModule from '../src/games/brain-even.js';
 import runGame from '../src/index.js';
 
 runGame(gameModule);
