f2d30cb98accbd2f4faf69042937208b301e52a8
alexeylozenko <alexeylozenko@gmail.com>
2021-04-19 00:44:40 +0300

    change args in function in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import readlineSync from 'readline-sync';
 
 /**
- * @param {object} gameModule
- * @param {number} quantityQuestions
+ * @param {object} game
+ * @param {number} roundsCount
  */
-export default (gameModule, quantityQuestions = 3) => {
+export default (game, roundsCount = 3) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  const descriptionGame = gameModule.getDescriptionGame();
+  const descriptionGame = game.getDescriptionGame();
   console.log(descriptionGame);
-  for (let i = 0; i < quantityQuestions; i += 1) {
-    const expression = gameModule.generateTask();
+  for (let i = 0; i < roundsCount; i += 1) {
+    const expression = game.generateTask();
     console.log(`Question: ${expression}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const normalizedAnswer = userAnswer.toLowerCase();
-    const totalResultOfGame = gameModule.playGame(expression, normalizedAnswer);
+    const totalResultOfGame = game.playGame(expression, normalizedAnswer);
     if (totalResultOfGame !== 'Correct!') {
       console.log(totalResultOfGame);
       console.log(`Let's try again, ${name}!`);
       break;
     }
-    if (i === quantityQuestions - 1) {
+    if (i === roundsCount - 1) {
       console.log(`Congratulations, ${name}!`);
       break;
     }
