3fee21f8837e6bd7e7704d53b63b64e4c76c9e89
alexeylozenko <alexeylozenko@gmail.com>
2021-05-03 11:19:05 +0300

    change export function

--- src/helpers.js
+++ src/helpers.js
@@ -3,8 +3,10 @@
  * @param {number} max
  * @returns {number}
  */
-export const getRandomNumber = (min = 1, max = 21) => {
+const getRandomNumber = (min = 1, max = 21) => {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
   return Math.floor(Math.random() * (maxValue - minValue) + minValue);
 };
+
+export default getRandomNumber;
